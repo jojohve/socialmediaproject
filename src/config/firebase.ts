a70f5675981a001ec66d9476d73fc89ae4afeea8
jojohve <joseph.laluna@outlook.it>
2025-01-30 13:35:23 +0100

    use form

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -33,3 +34,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
